53a4b34b3bdb810d7e006759fa26fbf1ada1b868
grotesqueg <denis_maths1@mail.ru>
2023-12-08 16:02:00 -0500

    commit

---
+++ src/TagExtractorRunner.java
@@ -0,0 +1,7 @@
+public class TagExtractorRunner {
+    public static void main(String[] args) {
+
+        TagExtractorFrame extractorFrame = new TagExtractorFrame();
+        System.out.println("Hello user!");
+    }
+}
